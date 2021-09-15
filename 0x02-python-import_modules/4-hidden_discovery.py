416f73bdee62a95d748ef873b122e442e2dea6bb
wealthjerry <afolabijeremayia@gmail.comgit config --global user.email afolabijeremayia@gmail.comgit config --global user.email afolabijeremayia@gmail.com>
2021-09-15 13:34:22 -0700

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+import hidden_4
 if __name__ == "__main__":
-    import hidden_4
     for i in dir(hidden_4):
-        if not i.startswith("__"):
-            print("{:s}".format(i)
+        if i[0:2] != "__":
+            print("{:s}".format(i))
+            
